eda531fd76573df9f64b017d37787af06318959f
Steven Papalia <stevenapapalia@yahoo.com>
2020-03-12 16:12:34 -0700

    Generated seed data for 300 bookings (still need to generate start/end dates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import properties from '../../faker';
+import { properties, bookings } from '../../faker';
 
 const App = () => (
   <div>
     <h1>Hello Everybody!</h1>
+    <div>BOOKING AT 0</div>
+    <div>{bookings[0].booking_id}</div>
+    <div>{bookings[0].property_id}</div>
+    <div>{bookings[0].start_date}</div>
+    <div>{bookings[0].end_date}</div>
+    <div>{bookings[0].adults}</div>
+    <div>{bookings[0].children}</div>
+    <div>{bookings[0].infants}</div>
     {properties.map((property) => {
       return (
         <div key={property.property_id}>
+          <div>PROPERTIES</div>
           <div>{property.property_id}</div>
           <div>{property.property_name}</div>
           <div>{property.price_per_night}</div>
--- faker.js
+++ faker.js
@@ -12,4 +12,26 @@ for (let i = 0; i < 100; i += 1) {
   });
 }
 
-export default properties;
+const bookings = [];
+for (let i = 0; i < properties.length; i += 1) {
+  for (let j = 0; j < 3; j += 1) {
+    const adults = Math.ceil(Math.random() * properties[i].max_occupants);
+    const children = Math.floor(Math.random() * (properties[i].max_occupants - adults));
+    const infants = Math.floor(Math.random() * (properties[i].max_occupants - adults - children));
+    const booking = {
+      booking_id: faker.random.uuid(),
+      property_id: properties[i].property_id,
+      start_date: 'start',
+      end_date: 'end',
+      adults,
+      children,
+      infants,
+    };
+    bookings.push(booking);
+  }
+}
+
+module.exports = {
+  properties,
+  bookings,
+};
