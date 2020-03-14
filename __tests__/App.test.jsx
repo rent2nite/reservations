9c80a6f9e8afed3924bfc65efe7e5ca958a8c770
Steven Papalia <stevenapapalia@yahoo.com>
2020-03-13 17:41:36 -0700

    Wrote tests in plain english

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -14,3 +14,28 @@ describe('Unit Tests For App Component', () => {
   test('write more tests like this ', () => {
   });
 });
+
+// INITIAL FORM
+// Should render a component with element pricePerNight
+// Price should match what it says in the db
+// Should render a component with elements dates
+// Should have startDate
+// Should have endDate
+// Onclick for startDate or endDate should make a calendar pop up
+// Should render a component with element guests
+// Should have a dropdown form of guest types (adults, children, infants)
+// Form buttons should increment and decrement correctly
+// Should not allow a user to add more guests than what is specified by max_occupants
+// Should have a close button
+// Should close when button is clicked, or user clicks outside of the form
+// Should render a component with element reserve
+// Onclick should send a POST req
+
+// FINAL FORM
+// Should display hypothetical price information when calendar is updated
+// PricePerNight X NightsStayed
+// CleaningFee
+// ServiceFee
+// OccupancyTaxesAndFees
+// Total
+// Should update calendar when form is submitted
---
+++ __tests__/Calendar.test.jsx
@@ -0,0 +1,14 @@
+// CALENDAR
+// Day of week should correspond with correct date
+// Calendar should display all days in current month
+// Calendar should display the correct number of days for each month
+// Calendar should have a button to change month
+// Calendar should display the correct data for each month when month is changed
+// Calendar should display the correct data when year is changed
+// Days that are booked should have a property of booked and different styling
+// Days that are blacked out should have a property of blacked_out and different styling
+// Days should change property from open to booked when a user makes a reservation
+// Only open days can be clicked
+// If an open day is clicked as startDate then an open day
+// is clicked as endDate, but in between the 2 dates there is a non-open date, does not work
+// Clear dates should clear all dates
